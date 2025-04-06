f80938cfa0ec99090b7639a0515bf821cc86058c
VatoAi <HorusNorway@proton.me>
2025-04-06 17:13:51 +0200

    service-worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,22 +1,23 @@
-self.addEventListener("install", (event) => {
-    event.waitUntil(
-      caches.open("snakkaz-cache-v1").then((cache) => {
-        return cache.addAll([
-          "/",
-          "/index.html",
-          "/manifest.json",
-          "/icons/snakkaz-icon-192.png",
-          "/icons/snakkaz-icon-512.png"
-        ]);
-      })
-    );
-  });
-  
-  self.addEventListener("fetch", (event) => {
-    event.respondWith(
-      caches.match(event.request).then((response) => {
-        return response || fetch(event.request);
-      })
-    );
-  });
-  
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('snakkaz-cache').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/icons/snakkaz-icon-192.png',
+        '/icons/snakkaz-icon-512.png',
+        '/src/main.tsx',
+        '/manifest.json',
+        // Legg til flere filer som skal caches her
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
